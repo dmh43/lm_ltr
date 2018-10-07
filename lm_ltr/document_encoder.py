29b3b5007ae4f40526581fc89ce61b56991cc956
Dany Haddad <danyhaddad43@gmail.com>
2018-10-07 11:05:01 -0500

    use bidirectional lstm

--- lm_ltr/document_encoder.py
+++ lm_ltr/document_encoder.py
@@ -8,8 +8,12 @@ class DocumentEncoder(nn.Module):
     super().__init__()
     self.document_token_embeds = document_token_embeds
     self.weights = nn.Embedding(len(document_token_embeds.weight), 1)
+    hidden_size = 100
     self.lstm = nn.LSTM(input_size=document_token_embeds.weight.shape[1],
-                        hidden_size=100)
+                        hidden_size=hidden_size,
+                        bidirectional=True,
+                        batch_first=True)
+    self.projection = nn.Linear(hidden_size * 2, 100)
     # MultiBatchRNN(bptt, max_seq, n_tok, emb_sz, n_hid, n_layers, pad_token=pad_token, bidir=bidir,
     #                              dropouth=dropouth, dropouti=dropouti, dropoute=dropoute, wdrop=wdrop, qrnn=qrnn)
 
@@ -20,7 +24,9 @@ class DocumentEncoder(nn.Module):
                                                                    packed_document[1])
     out, last_out_and_last_cell = self.lstm(packed_seq_document_tokens)
     last_out, last_cell_state = last_out_and_last_cell
-    doc_vecs = last_out.squeeze()[order]
+    batch_size = last_out.shape[1]
+    hidden_size = last_out.shape[2]
+    doc_vecs = self.projection(torch.cat([last_out[0], last_out[1]], 1)[order])
     return doc_vecs / (torch.norm(doc_vecs, 2, 1).unsqueeze(1) + 0.0001)
 
   def _weighted_forward(self, packed_document_and_order):
