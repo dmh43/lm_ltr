43cd1c3529fabf0bc6ab2307e60e03bd4b64baac
Dany Haddad <danyhaddad43@gmail.com>
2018-10-24 18:01:41 -0500

    abstract ranking

---
+++ lm_ltr/inference.py
@@ -0,0 +1,11 @@
+import torch
+
+def rank_documents(query, documents, ranker, doc_ids=None, k=None):
+  num_documents = len(documents)
+  if num_documents < k: raise ValueError('Not enough documents to rank')
+  doc_ids = doc_ids if doc_ids is not None else torch.arange(num_documents)
+  k = k if k is not None else num_documents
+  ranking_ids_for_batch = torch.squeeze(ranker(torch.unsqueeze(query, 0),
+                                               documents,
+                                               k))
+  return doc_ids[ranking_ids_for_batch]
