4065d576155d87956bf0ae4fc887f1c7f80903b1
Dany Haddad <danyhaddad43@gmail.com>
2018-11-03 10:13:40 -0500

    lstm document encoder with tbptt

--- lm_ltr/document_encoder.py
+++ lm_ltr/document_encoder.py
@@ -5,6 +5,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_sequence, pack_padded_sequence
 
+
+def repackage_hidden(h):
+  if isinstance(h, torch.Tensor):
+    return h.detach()
+  else:
+    return tuple(repackage_hidden(v) for v in h)
+
 class DocumentEncoder(nn.Module):
   def __init__(self,
                document_token_embeds,
@@ -31,36 +38,43 @@ class DocumentEncoder(nn.Module):
         self.pool = nn.AdaptiveMaxPool1d(1)
         self.projection = nn.Linear(num_filters, 100)
       elif self.use_lstm:
+        self.num_lstm_layers = 1
         self.lstm = nn.LSTM(input_size=word_embed_size,
                             hidden_size=self.lstm_hidden_size,
+                            num_layers=self.num_lstm_layers,
                             bidirectional=True,
                             batch_first=True)
-        self.projection = nn.Linear(self.lstm_hidden_size * 2, 100)
+        self.projection = nn.Linear(self._get_pooled_dim(), 100)
+
+  def _init_state(self, batch_size):
+    weight = next(self.lstm.parameters())
+    return (weight.new_zeros(self.num_lstm_layers, batch_size, self.lstm_hidden_size),
+            weight.new_zeros(self.num_lstm_layers, batch_size, self.lstm_hidden_size))
 
   def _lm_forward(self, document):
     seq_dim_first = torch.transpose(document, 0, 1)
     doc_vecs = self.pretrained_enc(seq_dim_first)
     return doc_vecs
 
-  def _lstm_forward(self, document, state, lens):
-    total_length = document.shape[1]
+  def _lstm_forward(self, document, lens):
+    pooled = self._pool(self._tbptt(document, lens))
+    doc_vecs = self.projection(pooled)
+    return doc_vecs
+
+  def _lstm(self, document, state, lens):
     document_tokens = self.document_token_embeds(document)
     packed_document_tokens = pack_padded_sequence(document_tokens, lens, batch_first=True)
-    out, last_out_and_last_cell = self.lstm(packed_document_tokens, state)
-    packed_last_out, last_cell_state = last_out_and_last_cell
-    last_out = pad_packed_sequence(packed_last_out, batch_first=True, total_length=total_length)
-    doc_vecs = self.projection(torch.cat([last_out[0], last_out[1]], 1))
-    return doc_vecs
+    return self.lstm(packed_document_tokens, state)
 
   def _tbptt(self, document, lens):
     bs, sl = document.size()
     outputs = []
-    state = init_state()
+    state = self._init_state(bs)
     for i in range(0, sl, self.bptt):
-      o, new_state = self._lstm_forward(document[:, i: min(i+self.bptt, sl)],
-                                         state,
-                                         lens[:, i: min(i+self.bptt, sl)])
-      state = repackage(new_state)
+      o, new_state = self._lstm(document[:, i: min(i+self.bptt, sl)],
+                                state,
+                                lens[:, i: min(i+self.bptt, sl)])
+      state = repackage_hidden(new_state)
       if i>(sl-self.max_seq):
         outputs.append(o)
     return self.concat(outputs)
