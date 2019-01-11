5741d67969023902c504aba63f6fa4b2f85856d0
Dany Haddad <danyhaddad43@gmail.com>
2019-01-11 17:56:19 +0200

    rankings are in terms of doc ids not doc titles

--- lm_ltr/eval_trec.py
+++ lm_ltr/eval_trec.py
@@ -18,14 +18,16 @@ def main():
   rankings_to_eval = read_query_test_rankings()
   qrels = parse_qrels()
   query_ids = list(set(qrels.keys()).intersection(set(rankings_to_eval.keys())))
-  ordered_qrels = [qrels[query] for query in query_ids]
-  ordered_rankings_to_eval = [rankings_to_eval[query] for query in query_ids]
   query_lookup = get_robust_test_queries()
   queries = [query_lookup[query_id] for query_id in query_ids]
   k = 10 if len(sys.argv) == 1 else int(sys.argv[1])
   document_lookup = read_cache(name('./doc_lookup.json', ['with_titles']), get_robust_documents_with_titles)
   document_title_to_id = read_cache('./document_title_to_id.json',
                                     lambda: print('failed'))
+  ordered_rankings_to_eval = [[document_title_to_id[title] for title in rankings_to_eval[query]]
+                              for query in query_ids]
+  ordered_qrels = [[document_title_to_id[title] for title in qrels[query]]
+                   for query in query_ids]
   document_id_to_title = _.invert(document_title_to_id)
   doc_ids = range(len(document_id_to_title))
   documents = [document_lookup[document_id_to_title[doc_id]] for doc_id in doc_ids]
