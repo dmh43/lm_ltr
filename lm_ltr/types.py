0fa938fceba1459ca5bdd812d15e7ef07ee7e2d6
Dany Haddad <danyhaddad43@gmail.com>
2019-01-26 14:31:09 -0600

    add basic mypy support

---
+++ lm_ltr/types.py
@@ -0,0 +1,10 @@
+from typing import List, Dict, Set, Tuple
+
+PairwiseBin = Set[Tuple[int, int]]
+PairwiseBins = Tuple[PairwiseBin, PairwiseBin]
+QueryPairwiseBins = Dict[str, PairwiseBins]
+QueryPairwiseBinsByRanker = Dict[str, QueryPairwiseBins]
+
+DocIdPair = Tuple[int, int]
+Query = List[int]
+TargetInfo = Tuple[DocIdPair, Query]
