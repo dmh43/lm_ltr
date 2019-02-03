f88a9c6cca0f05ec43a5a72630347e4dec48ff0d
Dany Haddad <danyhaddad43@gmail.com>
2019-02-03 13:03:45 -0600

    make sure only diff wrt requires grad params

--- lm_ltr/influence.py
+++ lm_ltr/influence.py
@@ -19,19 +19,20 @@ def calc_test_hvps(criterion: Callable,
                    test_dataset: Dataset,
                    collate_fn: Callable[[Any], torch.Tensor]):
   device = train_dataloader.device
+  diff_wrt = [p for p in trained_model.parameters() if p.requires_grad]
   hvp = HVP(calc_loss=lambda xs, target: criterion(trained_model(*xs), target),
-            parameters=[p for p in trained_model.parameters() if p.requires_grad],
+            parameters=diff_wrt,
             data=train_dataloader,
             num_batches=len(train_dataloader),
             damping=0.01)
   cg = CG(matmul=hvp,
-          result_len=sum(p.numel() for p in hvp.parameters),
-          max_iters=sum(p.numel() for p in hvp.parameters))
+          result_len=sum(p.numel() for p in diff_wrt),
+          max_iters=sum(p.numel() for p in diff_wrt))
   test_hvps = []
   for sample in test_dataset:
     x_test, label = to_device(collate_fn([sample]), device)
     loss_at_x_test = criterion(trained_model(*x_test), label.squeeze())
-    grads = autograd.grad(loss_at_x_test, trained_model.parameters())
+    grads = autograd.grad(loss_at_x_test, diff_wrt)
     grad_at_z_test = torch.cat([g.contiguous().view(-1) for g in grads])
     test_hvps.append(cg.solve(grad_at_z_test))
   return torch.stack(test_hvps)
@@ -43,8 +44,8 @@ def calc_influence(criterion: Callable,
                    test_hvps: torch.Tensor):
   features, target = train_sample
   train_loss = criterion(trained_model(*features), target)
-  params = trained_model.parameters()
-  grads = autograd.grad(train_loss, params)
+  diff_wrt = [p for p in trained_model.parameters() if p.requires_grad]
+  grads = autograd.grad(train_loss, diff_wrt)
   grad_at_train_sample = torch.cat([g.contiguous().view(-1) for g in grads])
   with torch.no_grad():
     influence = test_hvps.matmul(grad_at_train_sample)
