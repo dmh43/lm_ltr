ba210d0b2a231eb99ef9232aafc76d6f7e0ae432
Dany Haddad <danyhaddad43@gmail.com>
2019-02-10 10:06:57 -0600

    save sum of influence

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from lm_ltr.rel_score import RelScore
 from lm_ltr.regularization import Regularization
 from lm_ltr.snorkel_helper import Snorkeller
 from lm_ltr.globals import RANKER_NAME_TO_SUFFIX
-from lm_ltr.influence import get_num_neg_influences, calc_test_hvps
+from lm_ltr.influence import get_num_neg_influences, calc_test_hvps, calc_influence
 
 from rabbit_ml.rabbit_ml import Rabbit
 from rabbit_ml.rabbit_ml.arg_parsers import list_arg, optional_arg
@@ -525,13 +525,13 @@ def main():
       train_sample = train_dl.dataset[i]
       x, labels = to_device(collate_fn([train_sample]), device)
       device_train_sample = (x, labels.squeeze())
-      influences.append((i, get_num_neg_influences(multi_objective_model.loss,
-                                                   multi_objective_model.to(model_data.device),
-                                                   device_train_sample,
-                                                   test_hvps,
-                                                   diff_wrt=diff_wrt)))
+      influences.append((i, calc_influence(multi_objective_model.loss,
+                                           multi_objective_model.to(model_data.device),
+                                           device_train_sample,
+                                           test_hvps,
+                                           diff_wrt=diff_wrt).sum()))
     with open(rabbit.run_params.influences_path, 'w+') as fh:
-      json.dump([[train_dl.dataset[idx][1], num_neg_influences.item()] for idx, num_neg_influences in influences], fh)
+      json.dump([[train_dl.dataset[idx][1], influence.item()] for idx, influence in influences], fh)
 
 if __name__ == "__main__":
   import ipdb
