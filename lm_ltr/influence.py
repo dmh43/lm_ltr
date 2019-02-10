6c95d21c4514a960c5b78c76505668b65c6ddae3
Dany Haddad <danyhaddad43@gmail.com>
2019-02-09 20:13:30 -0600

    support using gauss newton for calculating influence

--- lm_ltr/influence.py
+++ lm_ltr/influence.py
@@ -9,6 +9,7 @@ from fastai.basic_data import DeviceDataLoader
 from fastai import to_device
 
 from .hvp import HVP
+from .gnp import GNP
 from .cg import CG
 from .grad_helpers import collect
 from .utils import maybe
@@ -23,13 +24,19 @@ def calc_test_hvps(criterion: Callable,
                    diff_wrt: Optional[torch.Tensor]=None):
   device = train_dataloader.device
   diff_wrt = maybe(diff_wrt, [p for p in trained_model.parameters() if p.requires_grad])
-  hvp = HVP(calc_loss=lambda xs, target: criterion(trained_model(*xs), target),
-            parameters=diff_wrt,
-            data=train_dataloader,
-            data_len=len(train_dataloader),
-            damping=0.01,
-            cache_batch=True)
-  cg = CG(matmul=hvp,
+  if run_params.use_gauss_newton:
+    matmul_class = GNP
+    damping = 0.0
+  else:
+    matmul_class = HVP
+    damping = 0.01
+  matmul = matmul_class(calc_loss=lambda xs, target: criterion(trained_model(*xs), target),
+                        parameters=diff_wrt,
+                        data=train_dataloader,
+                        data_len=len(train_dataloader),
+                        damping=damping,
+                        cache_batch=True)
+  cg = CG(matmul=matmul,
           result_len=sum(p.numel() for p in diff_wrt),
           max_iters=maybe(run_params['max_cg_iters'],
                           sum(p.numel() for p in diff_wrt)))
@@ -41,7 +48,7 @@ def calc_test_hvps(criterion: Callable,
     grad_at_z_test = collect(grads)
     test_hvps.append(cg.solve(grad_at_z_test,
                               test_hvps[-1] if len(test_hvps) != 0 else None))
-    hvp.clear_batch()
+    matmul.clear_batch()
   return torch.stack(test_hvps)
 
 
